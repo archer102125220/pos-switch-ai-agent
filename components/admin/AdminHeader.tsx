bb084ca334641869233d6f66c09eb72fa7b41d5f
Parker <archer102125220.2015@gmail.com>
2026-01-22 18:56:12 +0800

    fix: remove duplicate React import in AdminHeader
    The previous commit accidentally duplicated the React import statement, causing build errors.

--- components/admin/AdminHeader.tsx
+++ components/admin/AdminHeader.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect, useRef } from 'react';
-import { useState, useEffect, useRef } from 'react';
 import { Link, useRouter } from '@/i18n/navigation';
 import { classNames } from '@/utils/classNames';
 
